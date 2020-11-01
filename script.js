32208666e7184de2eeb9725266f27fbf342ea51b
joao13-vitoras <70807079+joao13-vitoras@users.noreply.github.com>
2020-11-01 13:14:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let nome = prompt('Digite seu nome:');
+let anoNascimento = parseInt(prompt('Digite seu ano de nascimento:'));
+function idade (a) {
+    const x = 2020;
+    y = x - a
+    return `${y} anos`;
+};
+alert (`Olá, seu nome é ${nome} e você tem` + idade(anoNascimento));
+
